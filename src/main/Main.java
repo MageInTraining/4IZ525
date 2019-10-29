29e92e8927cb6d14a43e46872e52aa46666aa17d
cen62777 <mistehlik@csas.cz>
2019-10-29 10:37:54 +0100

    it does something

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author cen62777
+ */
+public class Main{
+    
+    static String message = "This is a plaintext message, my friends!";
+    
+    public static void main(String[] args){
+        System.out.println(message);
+    }
+}
---
+++ src/tables/Tables.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tables;
+
+/**
+ *
+ * @author cen62777
+ */
+public class Tables {
+    
+    public int[][] INITIAL_PERMUTATION;
+    public int[][] FINAL_PERMUTATION;
+    public int[][] EXPANSION;
+    public int[][] P_PERMUTATION;
+    public int[][] PC_1;
+    public int[][] PC_2;
+    public int[][] S_BOX_1;
+    public int[][] S_BOX_2;
+    public int[][] S_BOX_3;
+    public int[][] S_BOX_4;
+    public int[][] S_BOX_5;
+    public int[][] S_BOX_6;
+    public int[][] S_BOX_7;
+    public int[][] S_BOX_8;
+    
+    
+}
